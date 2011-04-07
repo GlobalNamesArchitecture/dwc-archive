16ebdfc6fad6730c5b9c62b76efa6410284ad8f6
Dmitry Mozzherin <dmozzherin@gmail.com>
2011-04-07 16:48:42 -0400

    made specs working with rspec v > 2

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'dwc-archive'
 
-require 'spec/expectations'
+require 'rspec/expectations'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'dwc-archive'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 require 'ruby-debug'
 
-Spec::Runner.configure do |config|
-  
+RSpec.configure do |config|
+
 end
