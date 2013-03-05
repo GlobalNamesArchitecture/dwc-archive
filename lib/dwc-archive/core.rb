20a6e9cc1e4aa60b39bc07683177a58e8433ffce
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-03-05 10:35:21 -0500

    made id in the core to be optional

--- lib/dwc-archive/core.rb
+++ lib/dwc-archive/core.rb
@@ -9,8 +9,8 @@ class DarwinCore
       root_key = @archive.meta.keys[0]
       @data = @archive.meta[root_key][:core]
       raise DarwinCore::CoreFileError.new("Cannot find core in meta.xml, is meta.xml valid?") unless @data
-      @id = @data[:id][:attributes]
-      raise DarwinCore::CoreFileError.new("Cannot find core identifier") unless @id
+      @id = @data[:id][:attributes] 
+      # raise DarwinCore::CoreFileError.new("Cannot find core identifier") unless @id
       get_attributes(DarwinCore::CoreFileError)
     end
   end
