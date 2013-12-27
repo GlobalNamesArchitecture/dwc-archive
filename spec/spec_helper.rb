7ea2d6d16308a8ccb695ad37162f0318e0a3497b
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-12-27 10:12:03 -0500

    spec for eml generator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,31 @@ require 'socket'
 RSpec.configure do |config|
 end
 
+unless defined?(SPEC_CONSTANTS)
+  EML_DATA = {
+      id: '1234',
+      license: 'http://creativecommons.org/licenses/by-sa/3.0/',
+      title: 'Test Classification',
+      authors: [
+        { first_name: 'John',
+          last_name: 'Doe',
+          email: 'jdoe@example.com',
+          organization: 'Example',
+          position: 'Assistant Professor',
+          url: 'http://example.org' },
+          { first_name: 'Jane',
+            last_name: 'Doe',
+            email: 'jane@example.com' }
+    ],
+      metadata_providers: [
+        { first_name: 'Jim',
+          last_name: 'Doe',
+          email: 'jimdoe@example.com',
+          url: 'http://aggregator.example.org' }],
+      abstract: 'test classification',
+      citation:
+        'Test classification: Doe John, Doe Jane, Taxnonmy, 10, 1, 2010',
+      url: 'http://example.com'
+    }
+  SPEC_CONSTANTS = true
+end
